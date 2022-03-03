0c95cf2c0a26b735e09523a057b71052a0f7a870
Sanjeewa Kumara Narayana Narayana Mudiyansela <narayans@uoguelph.ca>
2022-03-03 13:01:42 -0500

    account creation : issue-log out button need to be fixed

---
+++ JobAppTracker/App.js
@@ -0,0 +1,39 @@
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreens from './src/screens/LoginScreens';
+import HomeScreen from './src/screens/HomeScreen';
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView style={styles.container} >
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreens} />
+            <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+
+        <ExpoStatusBar style="auto" />
+      </SafeAreaView >
+
+    </>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop:StatusBar.currentHeight,
+    // backgroundColor: 'green',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
