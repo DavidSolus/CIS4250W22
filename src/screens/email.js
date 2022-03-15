31c5fa5518adc6c977203a58a45b071b4b292c02
ajshields <43280342+ajshields@users.noreply.github.com>
2022-03-15 19:45:13 -0400

    Alex - Merging email read and write (#16)
    * Started working on a home page
    
    * Add buttons that navigate to the resume and home
    
    * Finish adding backend for local file upload
    
    * Cleanup some backend code, remove experiments
    
    * Rename DownloadFile to UploadFile
    
    * Temp add (doesn't work), playing with tab navigators
    
    * Add stable version of tab navigation for basic application/home/jobs screen
    
    * Fix whitespace
    
    * Can console log the files in the storage account
    
    * Add barebones for delete
    
    * Added email screen info
    
    Co-authored-by: David Solus <davidsolus@gmail.com>

---
+++ src/screens/email.js
@@ -0,0 +1,228 @@
+// React
+import { StyleSheet, Text, View, Button, Image, SafeAreaView, ScrollView, Modal, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import * as Google from 'expo-google-app-auth';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import * as MailComposer from 'expo-mail-composer';
+
+
+function EmailScreen({ navigation }) {
+  const [accessToken, setAccessToken] = React.useState();
+  const [userInfo, setUserInfo] = React.useState();
+  const [userEmailList, setUserEmailList] = React.useState();
+  const [userEmail, setUserEmail] = React.useState();
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const [sendTo, setSendTo] = React.useState();
+  const [sendSubject, setSendSubject] = React.useState();
+  const [sendBody, setSendBody] = React.useState();
+
+  //var arrayEmails = [{"id":"1","from":"ajshields@rogers.com","subject":"test","body":"this is the first email with lots of writing to see if itll go off page"},{"id":"2","from":"ashiel01@uoguelph.ca","subject":"test2","body":"this is the second email, sent from school email"},{"id":"3","from":"ajshields@rogers.com","subject":"hello","body":"hello world, i am alive"}];
+  var arrayEmails = [];
+
+  async function signInWithGoogleAsync() {
+    try {
+      const result = await Google.logInAsync ({
+        androidClientId: "313332122751-ollluipvbg60kumi9engkerqv553045n.apps.googleusercontent.com",
+        iosClientId: "313332122751-5gqn8c5ha4fivukqge8lrqr1ro1vh1dj.apps.googleusercontent.com",
+        scopes: ["https://mail.google.com/ https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/gmail.readonly"]
+      });
+
+      if(result.type === "success") {
+        setAccessToken(result.accessToken);
+      } else {
+        console.log("Permissions denied");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getUserData() {
+    //Get user profile data
+    let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${accessToken}`}
+    });
+    userInfoResponse.json().then(data => {
+      setUserInfo(data);
+    });
+
+    //Get user email list data
+    let userEmailListResponse = await fetch("https://gmail.googleapis.com/gmail/v1/users/"+userInfo.email+"/messages", {
+      headers: { Authorization: `Bearer ${accessToken}`}
+    });
+    var tempEmailList = [];
+    userEmailListResponse.json().then(data => {
+      for(var i = 0; i < data.messages.length; i++) {
+        tempEmailList.push(data.messages[i].id);
+      }
+      setUserEmailList(tempEmailList);
+    });
+
+    //Get user email info
+    var tempEmail = [];
+    for(var i = 0; i < userEmailList.length; i++) {
+      let userEmailResponse = await fetch("https://gmail.googleapis.com/gmail/v1/users/"+userInfo.email+"/messages/"+userEmailList[i], {
+        headers: { Authorization: `Bearer ${accessToken}`}
+      });
+      userEmailResponse.json().then(data => {
+          tempEmail.push(data);
+      });
+    }
+    setUserEmail(tempEmail);
+  }
+
+  function showUserInfo() {
+    if(userInfo) {
+      return (
+        <View style={styles.userInfo}>
+          <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
+          <Text>Welcome {userInfo.name}</Text>
+          <Text>{userInfo.email}</Text>
+        </View>
+      );
+    }
+  }
+  //console.log(jsonArray);
+
+  function showUserEmails() {
+    if(userInfo) {
+      if(userEmail) {
+        //loop through all email data and pick out important info
+        for(var i = 0; i < userEmail.length; i++) {
+          var tempFrom;
+          var tempSubject;
+          for(var j = 0; j < userEmail[i].payload.headers.length; j++) {
+            switch(userEmail[i].payload.headers[j].name) { //checks specific json headers for from and subject values
+              case "From":
+                tempFrom = userEmail[i].payload.headers[j].value;
+                break;
+              case "Subject":
+                tempSubject = userEmail[i].payload.headers[j].value;
+                break;
+              default:
+                break;
+            }
+          }
+          const tempJSON = { //create temp json to add to entire array
+            "id":i,
+            "from":tempFrom,
+            "subject":tempSubject,
+            "body":userEmail[i].snippet
+          }
+          arrayEmails.push(tempJSON);
+        }
+      }
+
+      const getEmails = arrayEmails => {
+        let content = [];
+        for (let i = 0; i < arrayEmails.length; i++) {
+          const item = arrayEmails[i];
+          content.push(<><Text key={item.id} style={styles.emailHeader}>From:</Text><Text>{item.from}</Text><Text style={styles.emailHeader}>Subject:</Text><Text>{item.subject}</Text><Text style={styles.emailHeader}>Message:</Text><Text>{item.body}</Text><Text style={{paddingTop:10, paddingBottom:15}}>_____________________________________________</Text></>);
+        }
+        return content;
+      };
+      return <View style={styles.emailInfo}>{getEmails(arrayEmails)}</View>;
+    }
+  }
+
+  function sendEmailButton() {
+    if(accessToken) {
+      return(
+         <Button title={"Send Mail"} onPress={() => setModalVisible(true)} />
+      );
+    }
+  }
+
+  async function sendEmailAsync() {
+    let result = await MailComposer.composeAsync({
+      recipients: [sendTo],
+      subject: sendSubject,
+      body: sendBody,
+    });
+  
+    alert(result.status);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        {showUserInfo()}
+        <Modal animationType='slide' visible={modalVisible} onRequestClose={() => setModalVisible(false)}>
+          <View style={styles.modal}>
+            <TextInput value={sendTo} onChangeText={(sendTo) => setSendTo(sendTo)} placeholder={'TO'} style={styles.textInput} />
+            <TextInput value={sendSubject} onChangeText={(sendSubject) => setSendSubject(sendSubject)} placeholder={'SUBJECT'} style={styles.textInput} />
+            <TextInput multiline value={sendBody} onChangeText={(sendBody) => setSendBody(sendBody)} placeholder={'COMPOSE EMAIL'} style={styles.textInput}/>
+            <Text>{'\n'}</Text>
+            <Button title={"Send"} onPress={sendEmailAsync} />
+            <Text>{'\n'}</Text>
+            <Button title={"Hide"} onPress={() => {setModalVisible(false); setSendTo(''); setSendSubject(''); setSendBody('')}} />
+          </View>
+        </Modal>
+        <View style={styles.buttons}>
+          <Button title={accessToken ? "View Inbox" : "Sign in with Google"} onPress={accessToken ? getUserData : signInWithGoogleAsync} />
+          {sendEmailButton()}
+        </View>
+        {showUserEmails()}
+        <Text>{'\n'}</Text>
+        <StatusBar style="auto" /> 
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  scrollView: {
+    backgroundColor: '#fff',
+    marginHorizontal: 20
+  },
+  modal: {
+    backgroundColor: '#fff',
+    margin: 50,
+    padding: 40,
+    borderRadius: 10,
+    flex: 1
+  },
+  buttons: {
+    paddingTop: 40,
+    paddingBottom: 20,
+    width: 'auto',
+    flex: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+  },
+  userInfo: {
+    paddingTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textInput: {
+    padding: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    backgroundColor: '#e8e8e8'
+  },
+  emailInfo: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginLeft: 20,
+    marginRight: 20
+  },
+  emailHeader: {
+    marginTop: 10,
+    fontWeight: 'bold'
+  },
+  profilePic: {
+    width: 50,
+    height: 50
+  }
+});
+
+export { EmailScreen };
--- src/screens/resume.js
+++ src/screens/resume.js
@@ -3,9 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system';
 // React
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Keyboard } from 'react-native';
 // Project
-import { ref, uploadBytes, uploadString } from "firebase/storage";
+import { ref, uploadBytes, list, listAll, deleteObject } from "firebase/storage";
 import { fbStorage } from '../db/server';
 
 // TODO: Get some sort of ID from user that is logged in and append that to file name
@@ -41,21 +41,80 @@ function UploadFile() {
 
 	return (
 		<Button
-			title="Select Document"
+			title="Upload Document"
 			onPress={_pickDocument}
 		/>
 	);
+}
+
+async function ListFile() {
+	// Reference Firebase Container
+	var filesInStorageList = [];
+	const listRef = ref(fbStorage, filePath);
+	console.log("Listing Files");
+
+
+	// List all files in container
+	listAll(listRef)
+		.then((res) => {
+			res.prefixes.forEach((folderRef) => {
+				// All the prefixes under listRef.
+				// You may call listAll() recursively on them.
+				console.log(res);
+			});
+			res.items.forEach((itemRef) => {
+				// All the items under listRef.
+				let fileInStorage = itemRef["_location"]["path_"].slice(filePath.length);
+				filesInStorageList.push(fileInStorage);
+				console.log(fileInStorage);
+			});
+		}).catch((error) => {
+			// Uh-oh, an error occurred!
+			console.log("ListFile - ERROR");
+		});
+		
+		//TODO: Render the files
+		return (
+			<View style={styles.MainContainer}>
+
+					{ filesInStorageList.map((item, key)=>(
+					<Text key={key} style={styles.TextStyle} onPress={ this.SampleFunction.bind(this, item) } > { item } </Text>)
+					)}
+
+			</View>
+		)
+}
+
+function DeleteFile() {
+	// Create a reference to the file to delete
+	const desertRef = ref(fbStorage, filePath+"colours.pdf");
+
+	// Delete the file
+	deleteObject(desertRef).then(() => {
+		// File deleted successfully
+	}).catch((error) => {
+		// Uh-oh, an error occurred!
+	});
+
+	return (
+		<TextInput
+			style={styles.textInput}
+			placeholder="File name to delete"
+			onBlur={Keyboard.dismiss}
+		/>
+	)
 }
 
 function ResumeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Resume Screen</Text>
-      <Button
-        title="Go to Home Screen"
-        onPress={() => navigation.navigate('Home')}
-      />
-	<UploadFile/>
+			<Button 
+				title="View Resumes"
+				onPress={() => ListFile()}
+			/>
+			<UploadFile/>
+			<DeleteFile/>
     </View>
   );
 }
@@ -67,6 +126,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+	MainContainer: {
+		flex: 1,
+		margin: 10
+	},
+	TextStyle:{
+		fontSize : 25,
+		 textAlign: 'center'
+	},
+	inputContainer: {
+		paddingTop: 15
+	},
+	textInput: {
+		borderColor: '#CCCCCC',
+		borderTopWidth: 1,
+		borderBottomWidth: 1,
+		height: 50,
+		fontSize: 25,
+		paddingLeft: 20,
+		paddingRight: 20
+	}
 });
 
 export { ResumeScreen }
