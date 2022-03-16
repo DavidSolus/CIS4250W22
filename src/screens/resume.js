2f4d44e10030e366901fba8d3bdf9ff0ec7fd479
David Solus <davidsolus@gmail.com>
2022-03-15 21:26:53 -0400

    Add experiment with components for viewing resumes

--- src/screens/resume.js
+++ src/screens/resume.js
@@ -3,7 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system';
 // React
-import { StyleSheet, Text, View, Button, TextInput, Keyboard } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Image, Animated, ScrollView, StyleSheet, Text, View, Button, TextInput, Keyboard, TouchableOpacity } from 'react-native';
 // Project
 import { ref, uploadBytes, list, listAll, deleteObject } from "firebase/storage";
 import { fbStorage } from '../db/server';
@@ -13,6 +14,79 @@ import { fbStorage } from '../db/server';
 const directory = "Resumes/";
 const UUID = "UUID_NUMBER/"; //TODO maybe change to metadeta
 const filePath = directory + UUID;
+
+class ResumeScreen extends Component {
+
+  constructor() {
+    super();
+
+    this.state = { valueArray: [], disabled: false }
+    this.index = 0;
+    this.animatedValue = new Animated.Value(0);
+  }
+
+  addMore = () => {
+    this.animatedValue.setValue(0);
+    let newlyAddedValue = { index: this.index }
+    this.setState({ disabled: true, valueArray: [...this.state.valueArray, newlyAddedValue] }, () => {
+      Animated.timing(
+        this.animatedValue,
+        {
+          toValue: 1,
+          duration: 500,
+          useNativeDriver: true
+        }
+      ).start(() => {
+        this.index = this.index + 1;
+        this.setState({ disabled: false });
+      });
+    });
+  }
+
+  render() {
+    const animationValue = this.animatedValue.interpolate(
+      {
+        inputRange: [0, 1],
+        outputRange: [-59, 0]
+      });
+
+    let newArray = this.state.valueArray.map((item, key) => {
+      if ((key) == this.index) {
+        return (
+          <Animated.View key={key} style={[styles.viewHolder, { opacity: this.animatedValue, transform: [{ translateY: animationValue }] }]}>
+            <Text style={styles.headerText}>Row {item.index}</Text>
+          </Animated.View>
+        );
+      }
+      else {
+        return (
+          <View key={key} style={styles.viewHolder}>
+            <Text style={styles.headerText}>Row No :- {item.index}</Text>
+						<TouchableOpacity activeOpacity={0.8} style={styles.buttonDesign} disabled={this.state.disabled} onPress={this.addMore}>
+							<Image source={require('../../assets/deleteButton.png')} style={styles.buttonImage} />
+						</TouchableOpacity>
+          </View>
+        );
+      }
+    });
+
+    return (
+      <View style={styles.container} >
+        <ScrollView>
+          <View style={{ flex: 1, padding: 4 }}>
+            {
+              newArray
+            }
+          </View>
+        </ScrollView>
+
+        <TouchableOpacity activeOpacity={0.8} style={styles.buttonDesign} disabled={this.state.disabled} onPress={this.addMore}>
+          <Image source={require('../../assets/deleteButton.png')} style={styles.buttonImage} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 function UploadFile() {
 	let _pickDocument = async () => {
@@ -105,27 +179,27 @@ function DeleteFile() {
 	)
 }
 
-function ResumeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Resume Screen</Text>
-			<Button 
-				title="View Resumes"
-				onPress={() => ListFile()}
-			/>
-			<UploadFile/>
-			<DeleteFile/>
-    </View>
-  );
-}
+// function ResumeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Resume Screen</Text>
+// 			<Button 
+// 				title="View Resumes"
+// 				onPress={() => ListFile()}
+// 			/>
+// 			<UploadFile/>
+// 			<DeleteFile/>
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 	MainContainer: {
 		flex: 1,
 		margin: 10
@@ -145,6 +219,34 @@ const styles = StyleSheet.create({
 		fontSize: 25,
 		paddingLeft: 20,
 		paddingRight: 20
+	},
+	container: {
+		flex: 1,
+	},
+	viewHolder: {
+		height: 55,
+		backgroundColor: '#ff4081',
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: 4
+	},
+	headerText: {
+		color: 'white',
+		fontSize: 25
+	},
+	buttonDesign: {
+		position: 'absolute',
+		right: 25,
+		bottom: 25,
+		borderRadius: 30,
+		width: 60,
+		height: 60,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	buttonImage: {
+		resizeMode: 'contain',
+		width: '100%',
 	}
 });
 
