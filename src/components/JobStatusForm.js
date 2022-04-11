409ef344c41906ed3f4fa94d4568cfd811f51084
Sanjeewa Kumara Narayana Narayana Mudiyansela <narayans@uoguelph.ca>
2022-04-11 16:52:12 -0400

    added push notifications

--- src/components/JobStatusForm.js
+++ src/components/JobStatusForm.js
@@ -1,12 +1,65 @@
-import { StyleSheet, Text, View, TouchableOpacity,KeyboardAvoidingView } from 'react-native'
-import React, { useContext, useState} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity,KeyboardAvoidingView, Platform } from 'react-native'
+import React, { useContext, useState, useRef, useEffect} from 'react'
 import { FormBuilder } from 'react-native-paper-form-builder';
 import { useForm } from 'react-hook-form';
 import { Button , Provider, TextInput} from 'react-native-paper';
 import { AuthContext } from '../contexts/AuthContext';
 import { JobStatusContext } from '../contexts/JobStatusContext';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+  });
 
 const JobStatusForm = ({navigation, route}) => {
+
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+    const notificationListener = useRef();
+    const responseListener = useRef();
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+        setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+        console.log(response);
+        });
+
+        return () => {
+        Notifications.removeNotificationSubscription(notificationListener.current);
+        Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
+    const pushNotification = async () => {
+        try {
+            await schedulePushNotification()
+        }catch(e){
+            console.log("Error: " + e)
+        }
+
+    }
+
+    const schedulePushNotification = async ()=> {
+        await Notifications.scheduleNotificationAsync({
+          content: {
+            title: "New job application status created! 📬",
+            body: 'Company Name: {companyName}',
+            data: { data: companyName },
+          },
+          trigger: { seconds: 2 },
+        });
+    }
 
     const {user} = useContext(AuthContext)
     const {createJobStatus} = useContext(JobStatusContext)
@@ -52,6 +105,7 @@ const JobStatusForm = ({navigation, route}) => {
                         style={styles.btn}
                         onPress = {()=>{
                             createJobStatus(data)
+                            pushNotification()
                             navigation.replace('JobStatus')
                             console.log('form data', data);
                         }}
@@ -240,6 +294,39 @@ const JobStatusForm = ({navigation, route}) => {
 }
 
 export default JobStatusForm
+
+
+
+const registerForPushNotificationsAsync = async ()=> {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
 
 const styles = StyleSheet.create({
     container: {
